4530aa7e6d21c03031243fd268c975f6c3a704db
chrisharb <c.harbord@hotmail.co.uk>
2021-09-14 16:13:07 +0100

    a

--- src/MurrellUS.jl
+++ src/MurrellUS.jl
@@ -2,7 +2,7 @@ module MurrellUS
 
 using DSP, HDF5, Dates, StatsBase, OldTools
 
-export getrootinfo, getdata, gettime_us, t_conv, trackarrivals, finddt
+export getrootinfo, getdata, gettime_us, t_conv, trackarrivals, finddt, trackarrivals_hpass
 
 include("US.jl")
 end
