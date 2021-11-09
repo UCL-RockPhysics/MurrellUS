1406ad42fbf55a608964548ad4f2a052d6630ace
chrisharby <c.harbord@hotmail.co.uk>
2021-11-09 15:22:08 +0000

    a

--- src/MurrellUS.jl
+++ src/MurrellUS.jl
@@ -1,6 +1,6 @@
 module MurrellUS
 
-using DSP, HDF5, Dates, StatsBase, OldTools
+using DSP, HDF5, Dates, StatsBase, OldTools, PyPlot
 
 export getrootinfo, getdata, gettime_us, t_conv, trackarrivals, finddt
 export plot_traces
