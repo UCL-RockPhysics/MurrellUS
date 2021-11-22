9431e326bfce1771bf8651cc08d68b018086a57f
chrisharby <c.harbord@hotmail.co.uk>
2021-11-22 15:15:43 +0000

    addition of new function to systematically correct for interface delays

--- src/US.jl
+++ src/US.jl
@@ -209,13 +209,63 @@ function finddt(s1,t1,s2,t2,f0,fcc,front,back)
     return DT, X
 end
 
-function plot_traces(fid::HDF5.File, info, chan; indices = 1:length(fid[info[:groups][chan]]), scale=0.01)
-    figure;
-    trace,delay = getdata(fid, info, chan, 1)
-    t = delay .+ (0.01:0.01:length(trace)/100)
+"""
+    plot_traces(fid::HDF5.File, info, chan; indices = 1:length(fid[info[:groups][chan]]), scale=0.1)
+Plot traces in a 2d line chart to check arrival time computation and visualisation.
+Input:
+    fid:    reference to open .hdf5 file in memory
+    info:   a dictionary containing information about the .hdf5 file
+    chan:   the channel of data to plot
+Optional input(s):
+    indices:indices or a range of traces to plot
+    scale:  the normalisation scale of the data
+Output:
+    gcf():  handle of the created plot
+# Usage:
+```
+    fig = plot_traces(fid, info, 3, indices = 1:10:1000, scale=0.1)
+```
+"""
+
+function plot_traces(fid::HDF5.File, info, chan; indices = 1:length(fid[info[:groups][chan]]), scale=0.1)
+    figure; # open new figure
+    trace,delay = getdata(fid, info, chan, 1) # open the first waveform to create a time vector for plotting
+    t = delay .+ (0.01:0.01:length(trace)/100) # create a time vector for plotting, in µs
     for k in indices
-        trace,delay = getdata(fid, info, chan, k)
-        plot(t,scale.*trace.+k, lw=0.5, c="k")
+        trace,delay = getdata(fid, info, chan, k) # get data
+        plot(t,scale.*trace.+k, lw=0.5, c="k") # plot data offsetting according to the trace number
     end
     return gcf()
+end
+
+
+function Vcalc(P, info, range)
+    pathUS = "/Users/christopherharbord/Dropbox/Research/UCL/raw_lab data/Murrell_US_data/"
+    experiment=info[:name] # Get experiment name
+    i = info[:I_US] # get indices of waveform data
+    fid = h5open(pathUS*experiment*".h5") # Open .hdf5 file containing waveforms
+    info_US = getrootinfo(fid) # Get information about file structure
+    t_us = gettime_us(fid, info_US, 3) # get ultrasonic scan time for interpolation function
+    M_interp!(P, t_us) # interpolate mechanical data for plotting and corrections
+    master,~ = getdata(fid, info_US, 3, i[1]) # get the master waveform, corresponding to the experiment hit point
+    P[:DT] = trackarrivals(fid, info_US, 3, master, info[:t1], 100e6, 1000e6, 50, 50, indices=i) # run the cross correlation algorithm to obtain arrival time
+
+    ## Corrections for load and confining pressure
+    P[:L_samp_m] = (1 .-(P[:ε_i][i] .-P[:ε_i][i[1]]))*info[:L_mm]*1e-3 # get sample length during experiment
+    # Estimate length of pistons, subtracting confining pressure contribution and load contribution, both estimated from load cycles
+    P[:L_ass_m] = -info[:L0].*(P[:σ3_MPa_i][i]./200e3) .- # Confining pressure contribution
+                (P[:F_kN_i][i]*0.01e-3) .+# Load contribution
+                info[:L0]
+    P[:T_ass_s] = P[:L_ass_m]./info[:V_ass] # Estimate travel time through pistons as a function of time
+    P[:T_ini_s] = 40e-6 +info[:t0] .-P[:T_ass_s][1] # Estimate initial travel time through sample
+    P[:V_ini] = P[:L_samp_m][1]/P[:T_ini_s] # estimate initial sample wavespeed
+    x = P[:F_kN_i][i[range[1]]:i[range[end]]] # get force data to estimate interface delay
+    y = P[:DT][range] .+P[:L_samp_m][range]./P[:V_ini] .+P[:T_ass_s][range] # get delay time to estimate interface delay
+    P[:m],_ = linfit(x,y) # fit DT as a function of load for interface correction
+
+    ## Final velocity calculations
+    P[:T_samp] = 40e-6 +t0 .-P[:T_ass_s] .-(P[:DT] -P[:F_kN_i][i]*P[:m]) # correct travel time through sample for all delays
+    V = P[:L_samp_m]./P[:T_samp] # compute velocity based on corrected travel time
+    P[:V_ms] = V#./V[1] # store the velocity output
+    P[:ΔV] = (V./V[1]).-1 # store the normalised velocity output
 end
